caeb6e1b553ceac9d33cc636bf6e63f114e9f623
Swikrit Bhatta <swikritb@gmail.com>
2023-09-01 21:39:52 +0545

    task8

---
+++ task8.js
@@ -0,0 +1,10 @@
+function checkingUpperCase(input) {
+  if (input === input.toUpperCase()) {
+    console.log("it is uppper case");
+  } else {
+    console.log("it is lower case");
+  }
+}
+
+checkingUpperCase("hello");
+checkingUpperCase("HELLO");
